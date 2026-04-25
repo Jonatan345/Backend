0e2ab69f289f655d2ab4957ea4ccb4aece5084f6
Jonatan345 <imanuel.jonatan@student.pradita.ac.id>
2026-04-25 07:22:40 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,26 +1,10 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// prisma/client.js
+require('dotenv').config();
 
-const ws = require('ws');
-const { neonConfig } = require('@neondatabase/serverless');
 const { PrismaClient } = require('@prisma/client');
-const { Pool } = require('@neondatabase/serverless');
-const { PrismaNeon } = require('@prisma/adapter-neon');
-
-neonConfig.webSocketConstructor = ws;
-
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error('Missing DATABASE_URL in environment. Add it to .env or set it before running the server.');
-}
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
 
 const prisma = new PrismaClient({
-  adapter,
-  log: ['query', 'info', 'warn', 'error'],
+  log: ['error']
 });
 
 module.exports = prisma;
-
